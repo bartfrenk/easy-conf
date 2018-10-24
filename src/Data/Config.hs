5dafdb3b6283e95d13a885af40668de8411c3ae9
Bart Frenk <bart.frenk@gmail.com>
2018-10-24 21:43:08 +0200

    Add convenience functions

--- src/Data/Config.hs
+++ src/Data/Config.hs
@@ -6,11 +6,15 @@ module Data.Config
   , envPlugin
   , idPlugin
   , makePlugin
+  , readValue
+  , extractConfig
   ) where
 
 import           Control.Monad.Catch (MonadThrow)
-import           Data.Aeson          (FromJSON)
+import           Control.Monad.Trans (MonadIO, liftIO)
+import           Data.Aeson          (FromJSON, Value)
 import qualified Data.ByteString     as B
+import           Data.Text           (Text)
 
 import           Data.Config.Eval
 import           Data.Config.Plugin  (MonadEnv (..), Plugin, envPlugin,
@@ -24,4 +28,11 @@ decodeWithDefault = decodeWithPlugin defaultPlugin
 
 defaultPlugin :: MonadEnv m => Plugin m
 defaultPlugin = envPlugin <> idPlugin
+
+readValue :: (MonadIO m, MonadThrow m) => FilePath -> m Value
+readValue path = liftIO $ B.readFile path >>= throwDecode
+
+extractConfig :: (MonadEnv m, MonadThrow m) => FromJSON a => Text -> Value -> m a
+extractConfig key = decodeWithPluginNested defaultPlugin [key]
+
 
